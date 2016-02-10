5660f8061aa98e37fce9d408edd8d674700b8c4e
Afton Downey <aaike001@gmail.com>
2016-02-10 13:20:37 -0800

    add task class

--- app/app.php
+++ app/app.php
@@ -1,12 +1,9 @@
 <?php
 
 		require_once __DIR__."/../vendor/autoload.php";
+		require_once __DIR__."/../src/Task.php"
 
 		$app = new Silex/Application();
-
-		$app->get("/", function() {
-			return "home";
-		});
 
 		return $app;
 
